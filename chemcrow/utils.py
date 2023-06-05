e8182a0519a1c6a581ab0edfa446f6fa01de2ec1
doncamilom <doncamilom@gmail.com>
2023-06-05 16:08:52 +0200

    adding utils

---
+++ chemcrow/utils.py
@@ -0,0 +1,26 @@
+from rdkit import Chem
+
+
+def is_smiles(text):
+    try:
+        m = Chem.MolFromSmiles(text, sanitize=False)
+        if m is None:
+            return False
+        return True
+    except:
+        return False
+
+def largest_mol(smiles):
+    ss = smiles.split(".")
+    ss.sort(key=lambda a: len(a))
+    while not is_smiles(ss[-1]):
+        rm = ss[-1]
+        ss.remove(rm)
+    return ss[-1]
+
+def canonical_smiles(smiles):
+    try:
+        smi = Chem.MolToSmiles(Chem.MolFromSmiles(smiles), canonical=True)
+        return smi
+    except Exception as e:
+        return "Invalid SMILES string"
