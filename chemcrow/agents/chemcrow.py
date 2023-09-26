3caac636ddf440eddda76cd14d6d1e4333a9f9b5
avanteijlingen <57176642+avanteijlingen@users.noreply.github.com>
2023-09-26 22:49:26 +0200

    modified _make_llm
    To allow for loading llamaCpp and GPT4ALL models

--- chemcrow/agents/chemcrow.py
+++ chemcrow/agents/chemcrow.py
@@ -1,34 +1,61 @@
-import langchain
+import langchain, os
 import nest_asyncio
 from langchain import PromptTemplate, chains
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from rmrkl import ChatZeroShotAgent, RetryAgentExecutor
 
+
+from langchain.llms import LlamaCpp, GPT4All
+
 from .prompts import FORMAT_INSTRUCTIONS, QUESTION_PROMPT, REPHRASE_TEMPLATE, SUFFIX
 from .tools import make_tools
 
 
-def _make_llm(model, temp, verbose, api_key):
+def _make_llm(model, temp, verbose, api_key, max_tokens=1000, n_ctx=2048):
     if model.startswith("gpt-3.5-turbo") or model.startswith("gpt-4"):
-        llm = langchain.chat_models.ChatOpenAI(
-            temperature=temp,
-            model_name=model,
-            request_timeout=1000,
-            streaming=True if verbose else False,
-            callbacks=[StreamingStdOutCallbackHandler()] if verbose else [None],
-            openai_api_key = api_key
-        )
+        try:
+            llm = langchain.chat_models.ChatOpenAI(
+                temperature=temp,
+                model_name=model,
+                request_timeout=1000,
+                streaming=True if verbose else False,
+                callbacks=[StreamingStdOutCallbackHandler()] if verbose else [None],
+                openai_api_key = api_key
+            )
+        except:
+            return "Invalid openai key"
     elif model.startswith("text-"):
-        llm = langchain.OpenAI(
-            temperature=temp,
-            model_name=model,
-            streaming=True if verbose else False,
-            callbacks=[StreamingStdOutCallbackHandler()] if verbose else [None],
-            openai_api_key = api_key
-        )
+        try:
+            llm = langchain.OpenAI(
+                temperature=temp,
+                model_name=model,
+                streaming=True if verbose else False,
+                callbacks=[StreamingStdOutCallbackHandler()] if verbose else [None],
+                openai_api_key = api_key
+            )
+        except:
+            return "Invalid openai key"
+    elif os.path.exists(model):
+        ext = os.path.splitext(model)[-1].lower()
+        if ext == ".bin":
+            # Assuming this is a GPT4ALL style set of tensors
+            llm = GPT4All(model=model, max_tokens=max_tokens, backend='gptj', verbose=False)
+        elif ext == ".gguf":
+            # Assuming this is a LlamaCpp style set of tensors
+            llm = LlamaCpp(
+                model_path=model,
+                temperature=temp,
+                max_tokens=max_tokens,
+                n_ctx=n_ctx,
+                top_p=1,
+                verbose=True, # Verbose is required to pass to the callback manager
+            )
+        else:
+            raise ValueError(f"Found file: {model}, but this function is only able to load .bin and .gguf models.")    
     else:
         raise ValueError(f"Invalid model name: {model}")
     return llm
+
 
 
 class ChemCrow:
@@ -41,15 +68,18 @@ class ChemCrow:
         max_iterations=40,
         verbose=True,
         openai_api_key: str = None,
-        api_keys: dict = None
+        api_keys: dict = None,
+        max_tokens: int = 1000, # Not required for using OpenAI's API
+        n_ctx: int =  2048
     ):
-        try:
-            self.llm = _make_llm(model, temp, verbose, openai_api_key)
-        except:
-            return "Invalid openai key"
 
+        self.llm = _make_llm(model, temp, verbose, openai_api_key, max_tokens, n_ctx)
+        
+        if isinstance(self.llm, str):
+            return self.llm
+        
         if tools is None:
-            tools_llm = _make_llm(tools_model, temp, verbose, openai_api_key)
+            tools_llm = _make_llm(tools_model, temp, max_tokens, verbose, openai_api_key)
             tools = make_tools(
                 tools_llm,
                 api_keys = api_keys,
