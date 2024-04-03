dc5d8fa56d58a7121f7be23851496aedd72ed5ff
ajaymur91 <15915423+ajaymur91@users.noreply.github.com>
2024-04-03 15:49:04 -0400

    azure for llm

---
+++ chemcrow/chemcrow.py
@@ -0,0 +1,109 @@
+from typing import Optional
+import os
+from azure.identity import EnvironmentCredential
+
+from langchain.chat_models import AzureChatOpenAI
+
+GENAI_PROXY = "https://genai-platform-dev.pg.com/stg/v1"
+COGNITIVE_SERVICES = "https://cognitiveservices.azure.com/.default"
+OPEN_API_VERSION = "2023-05-15"
+# Replace userid with a real username (a person actually providing the prompt)
+# or service principal id in case of non-personal context.
+# Replace also project-name to your project name.
+HEADERS = {
+    "userid": "muralidharan.a.2@pg.com",
+    "project-name": "ChatMDS",
+}
+
+from dotenv import load_dotenv 
+load_dotenv("/Users/muralidharan.a.2/.crewkey_azure", override=True)
+env_credential = EnvironmentCredential()
+token = env_credential.get_token(COGNITIVE_SERVICES).token
+
+import langchain
+from dotenv import load_dotenv
+from langchain import PromptTemplate, chains
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from pydantic import ValidationError
+from rmrkl import ChatZeroShotAgent, RetryAgentExecutor
+
+from .prompts import FORMAT_INSTRUCTIONS, QUESTION_PROMPT, REPHRASE_TEMPLATE, SUFFIX
+from .tools import make_tools
+
+
+def _make_llm(model, temp, api_key, streaming: bool = False):
+    if model.startswith("gpt-3.5-turbo") or model.startswith("gpt-4"):
+        #llm = langchain.chat_models.ChatOpenAI(
+        llm = AzureChatOpenAI(
+    		openai_api_version=OPEN_API_VERSION,
+    		azure_deployment="GPT-4-32K",
+    		azure_endpoint=GENAI_PROXY,
+    		api_key=token,
+    		default_headers=HEADERS,
+            temperature=temp,
+            request_timeout=1000,
+            streaming=streaming,
+            callbacks=[StreamingStdOutCallbackHandler()],
+        )
+    #elif model.startswith("text-"):
+    #    llm = langchain.OpenAI(
+    #        temperature=temp,
+    #        model_name=model,
+    #        streaming=streaming,
+    #        callbacks=[StreamingStdOutCallbackHandler()],
+    #        openai_api_key=api_key,
+    #    )
+    else:
+        raise ValueError(f"Invalid model name: {model}")
+    return llm
+
+
+class ChemCrow:
+    def __init__(
+        self,
+        tools=None,
+        model="gpt-4-0613",
+        tools_model="gpt-3.5-turbo-0613",
+        temp=0.1,
+        max_iterations=40,
+        verbose=True,
+        streaming: bool = True,
+        openai_api_key: Optional[str] = None,
+        api_keys: dict = {},
+    ):
+        """Initialize ChemCrow agent."""
+
+        load_dotenv()
+        try:
+            self.llm = _make_llm(model, temp, openai_api_key, streaming)
+        except ValidationError:
+            raise ValueError("Invalid OpenAI API key")
+
+        if tools is None:
+            api_keys["OPENAI_API_KEY"] = openai_api_key
+            tools_llm = _make_llm(tools_model, temp, openai_api_key, streaming)
+            tools = make_tools(tools_llm, api_keys=api_keys, verbose=verbose)
+
+        # Initialize agent
+        self.agent_executor = RetryAgentExecutor.from_agent_and_tools(
+            tools=tools,
+            agent=ChatZeroShotAgent.from_llm_and_tools(
+                self.llm,
+                tools,
+                suffix=SUFFIX,
+                format_instructions=FORMAT_INSTRUCTIONS,
+                question_prompt=QUESTION_PROMPT,
+            ),
+            verbose=True,
+            max_iterations=max_iterations,
+        )
+
+        rephrase = PromptTemplate(
+            input_variables=["question", "agent_ans"], template=REPHRASE_TEMPLATE
+        )
+
+        self.rephrase_chain = chains.LLMChain(prompt=rephrase, llm=self.llm)
+
+    def run(self, prompt):
+        outputs = self.agent_executor({"input": prompt})
+        return outputs["output"]
