f276e6558e5f27648d7e040a28cd874c3de48554
Ajay Muralidharan <15915423+ajaymur91@users.noreply.github.com>
2024-04-03 10:58:09 -0400

    Update __init__.py

--- chemcrow/tools/__init__.py
+++ chemcrow/tools/__init__.py
@@ -2,7 +2,7 @@
 
 from .rdkit import *  # noqa
 from .search import *  # noqa
-from .rxn4chem import *  # noqa
+#from .rxn4chem import *  # noqa
 from .safety import *  # noqa
 from .chemspace import *  # noqa
 from .converters import *  # noqa
