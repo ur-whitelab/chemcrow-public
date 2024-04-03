43721a219597dd3f15035bdf598c9d9c18f54412
ajaymur91 <15915423+ajaymur91@users.noreply.github.com>
2024-04-03 13:23:10 -0400

    mds_tools

--- chemcrow/agents/tools.py
+++ chemcrow/agents/tools.py
@@ -1,5 +1,5 @@
 import os
-
+import inspect
 from langchain import agents
 from langchain.base_language import BaseLanguageModel
 
@@ -51,6 +51,21 @@ def make_tools(llm: BaseLanguageModel, api_keys: dict = {}, verbose=True):
         #     semantic_scholar_api_key=semantic_scholar_api_key
         # ),
     ]
+
+    # Get absolute path of the directory where this script is located
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+
+    # Construct the path to the data.csv file relative to the script directory
+    data_file_path = os.path.join(script_dir, '../tools/create/data.csv')
+
+    # Add mds tools and hsp tool
+    mds_classes = inspect.getmembers(mds, inspect.isclass)
+    hsp_classes = inspect.getmembers(hsp, inspect.isclass)
+    mds_tools = [cls for name, cls in mds_classes if issubclass(cls, BaseTool) and cls is not BaseTool]
+    hsp_tools = [cls for name, cls in hsp_classes if issubclass(cls, BaseTool) and cls is not BaseTool]
+    all_tools += [tool() for tool in mds_tools]
+    all_tools += [tool() for tool in hsp_tools]
+
     if chemspace_api_key:
         all_tools += [GetMoleculePrice(chemspace_api_key)]
     if serp_api_key:
