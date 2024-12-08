90f10a8a647dfab3deac856392d90f7fe8417999
Kota-Yamaguchi <yama.guchi.k1207@gmail.com>
2024-12-08 14:06:21 +0900

    modify _make_llm into _make_llm_dify

--- chemcrow/agents/chemcrow.py
+++ chemcrow/agents/chemcrow.py
@@ -1,47 +1,21 @@
 import os
 
-import langchain_anthropic
-import langchain_openai
-
 # import langchain
 from dotenv import load_dotenv
 from langchain import PromptTemplate, chains
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from pydantic import ValidationError
 
 from rmrkl import ChatZeroShotAgent, RetryAgentExecutor
 
+from .dify_llm import DifyCustomLLM
 from .prompts import FORMAT_INSTRUCTIONS, QUESTION_PROMPT, REPHRASE_TEMPLATE, SUFFIX
 from .tools import make_tools
 
 
-def _make_llm(model, temp, api_keys, streaming: bool = False):
-    if model.startswith("gpt-4o-mini") or model.startswith("gpt-4"):
-        llm = langchain_openai.ChatOpenAI(
-            temperature=temp,
-            model_name=model,
-            request_timeout=1000,
-            streaming=streaming,
-            callbacks=[StreamingStdOutCallbackHandler()],
-            api_key=api_keys['OPENAI_API_KEY'],
-        )
-    elif model.startswith("text-"):
-        raise NotImplementedError("Text models are not supported yet")
-        # llm = langchain.OpenAI(
-        #     temperature=temp,
-        #     model_name=model,
-        #     streaming=streaming,
-        #     callbacks=[StreamingStdOutCallbackHandler()],
-        #     openai_api_key=api_keys['OPENAI_API_KEY'],
-        # )
-    elif model.startswith('claude'):
-        llm = langchain_anthropic.ChatAnthropic(
-            temperature=temp,
-            model_name=model,
-            streaming=streaming,
-            callbacks=[StreamingStdOutCallbackHandler()],
-            api_key=api_keys['ANTHROPIC_API_KEY'],
-        )
+def _make_dify_llm(model, temp, api_key, base_url, streaming: bool = False):
+    if  model.startswith("dify"):
+        llm =DifyCustomLLM(api_key=api_key, user_id="llm-study-2", base_url=base_url)
+    
     else:
         raise ValueError(f"Invalid model name: {model}")
     return llm
@@ -51,8 +25,10 @@ class ChemCrow:
     def __init__(
         self,
         tools=None,
-        model="gpt-4-0613",
-        tools_model="gpt-4o-mini",
+        # model="gpt-4-0613",
+        model="dify",
+        tools_model="dify",
+        # tools_model="gpt-4o-mini",
         temp=0.1,
         max_iterations=40,
         verbose=True,
@@ -63,19 +39,15 @@ class ChemCrow:
 
         dotenv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
         load_dotenv(dotenv_path)
-
-        openai_api_key = api_keys.get('OPENAI_API_KEY') or os.getenv('OPENAI_API_KEY')
-        print(f"Loading .api_keys file from {dotenv_path}",api_keys)
-        if openai_api_key is None:
-            raise ValueError('OPENAI_API_KEY が設定されていません。.envファイルを確認してください。')
-
+        dify_api_key = api_keys.get("DIFY_API_KEY") or os.getenv("DIFY_API_KEY")
+        dify_base_url = api_keys.get("DIFY_BASE_URL") or os.getenv("DIFY_BASE_URL")
         try:
-            self.llm = _make_llm(model, temp, api_keys, streaming)
+            self.llm = _make_dify_llm(model, temp, dify_api_key,dify_base_url, streaming)
         except ValidationError:
             raise ValueError("Invalid OpenAI API key")
 
         if tools is None:
-            tools_llm = _make_llm(tools_model, temp, api_keys, streaming)
+            tools_llm = _make_dify_llm(tools_model, temp, dify_api_key,dify_base_url, streaming)
             tools = make_tools(tools_llm, api_keys=api_keys, verbose=verbose)
 
         # Initialize agent
