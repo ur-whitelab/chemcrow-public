39dbc84d6bb75a66af5fe1842625b38e234c5e6d
Andres M Bran <doncamilom@gmail.com>
2024-12-19 18:24:24 +0100

    fix local retro tool

--- chemcrow/tools/reactions.py
+++ chemcrow/tools/reactions.py
@@ -65,16 +65,58 @@ class RXNRetrosynthesisLocal(BaseTool):
         if not is_smiles(reactants):
             return "Incorrect input."
 
-        prediction_id = self.predict_reaction(reactants)
-        results = self.get_results(prediction_id)
-        product = results["productMolecule"]["smiles"]
-        return product
+        paths = self.retrosynthesis(reactants)
+        procedure = self.get_action_sequence(paths[0])
+        return procedure
 
-    def predict_reaction(self, reactants: str) -> str:
+    def retrosynthesis(self, reactants: str) -> str:
         """Make api request."""
         response = requests.post(
-            "http://localhost:8021/api/v1/run",
+            "http://localhost:8052/api/v1/run",
             headers={"Content-Type": "application/json"},
             data=json.dumps({"smiles": reactants})
         )
-        return response.json()['product'][0]
+        return response.json()
+
+    def get_action_sequence(self, path):
+        """Get sequence of actions."""
+        actions = path
+        json_actions = self._preproc_actions(actions)
+        llm_sum = self._summary_gpt(json_actions)
+        return llm_sum
+
+    def _preproc_actions(self, path):
+        """Preprocess actions."""
+        def _clean_actions(d):
+            if 'metadata' in d:
+                if 'mapped_reaction_smiles' in d['metadata']:
+                    r = d['metadata']['mapped_reaction_smiles'].split(">>")
+                    yield {"reactants": r[1], "products": r[0]}
+            if 'children' in d:
+                for c in d['children']:
+                    yield from _clean_actions(c)
+
+        rxns = list(_clean_actions(path))
+        return rxns
+
+    def _summary_gpt(self, json: dict) -> str:
+        """Describe synthesis."""
+        llm = ChatOpenAI(  # type: ignore
+            temperature=0.05,
+            model_name="gpt-3.5-turbo-16k",
+            request_timeout=2000,
+            max_tokens=2000,
+            openai_api_key=self.openai_api_key,
+        )
+        prompt = (
+            "Here is a chemical synthesis described as a json.\nYour task is "
+            "to describe the synthesis, as if you were giving instructions for"
+            "a recipe. Use only the substances, quantities, temperatures and "
+            "in general any action mentioned in the json file. This is your "
+            "only source of information, do not make up anything else. Also, "
+            "add 15mL of DCM as a solvent in the first step. If you ever need "
+            'to refer to the json file, refer to it as "(by) the tool". '
+            "However avoid references to it. \nFor this task, give as many "
+            f"details as possible.\n {str(json)}"
+        )
+        return llm([HumanMessage(content=prompt)]).content
